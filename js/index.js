da0b37bec5a8ca02386a5df4b4f97646faa34602
BrandonSaravia <BPSaravia@gmail.com>
2019-03-04 17:39:46 -0500

    referenced all content from header to main content into the html from site-content object

--- js/index.js
+++ js/index.js
@@ -40,3 +40,53 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+///////////////////////////////////////////////////////// Nav
+let navItems = siteContent['nav'];
+const allHrefs = document.querySelectorAll('a');
+allHrefs[0].textContent = navItems['nav-item-1'];
+allHrefs[1].textContent = navItems['nav-item-2'];
+allHrefs[2].textContent = navItems['nav-item-3'];
+allHrefs[3].textContent = navItems['nav-item-4'];
+allHrefs[4].textContent = navItems['nav-item-5'];
+allHrefs[5].textContent = navItems['nav-item-6'];
+
+////////////////////////////////////////////////////////// Cta
+let ctaChild = siteContent['cta'];
+
+/////// h1
+let ctaH1 = document.getElementsByTagName('h1');
+ctaH1[0].textContent = ctaChild['h1'];
+
+/////// button
+let ctaButton = document.getElementsByTagName('button');
+ctaButton[0].textContent = ctaChild['button'];
+
+/////// img
+let ctaImgSrc = document.getElementById("cta-img");
+ctaImgSrc.setAttribute('src', siteContent["cta"]["img-src"])
+
+///////////////////////////////////////////////////////////// Main-Content
+let mainContentChild = siteContent['main-content'];
+
+/////// h4
+const mainContentH4s = document.querySelectorAll('h4');
+mainContentH4s[0].textContent = mainContentChild['features-h4'];
+mainContentH4s[1].textContent = mainContentChild['about-h4'];
+mainContentH4s[2].textContent = mainContentChild['services-h4'];
+mainContentH4s[3].textContent = mainContentChild['product-h4'];
+mainContentH4s[4].textContent = mainContentChild['vision-h4'];
+
+/////// content
+const mainContentP = document.querySelectorAll('p');
+mainContentP[0].textContent = mainContentChild['features-content'];
+mainContentP[1].textContent = mainContentChild['about-content'];
+mainContentP[2].textContent = mainContentChild['services-content'];
+mainContentP[3].textContent = mainContentChild['product-content'];
+mainContentP[4].textContent = mainContentChild['vision-content'];
+
+/////// img
+let mainContentImgSrc = document.getElementById("middle-img");
+mainContentImgSrc.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+////////////////////////////////////////////////////////////// Contact
